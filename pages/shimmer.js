ff2adfccadd91b6c49e5e743888133037c53893e
guswls1419 <guswns1419@naver.com>
2022-08-05 00:54:01 +0900

    Image-Optimization

--- pages/shimmer.js
+++ pages/shimmer.js
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import ViewSource from '../components/view-source'
+import Image from "next/image";
+import ViewSource from "../components/view-source";
 
 const shimmer = (w, h) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -8,17 +8,17 @@ const shimmer = (w, h) => `
       <stop stop-color="#333" offset="20%" />
       <stop stop-color="#222" offset="50%" />
       <stop stop-color="#333" offset="70%" />
-    </linearGradient>
+    </linearGradient> 
   </defs>
   <rect width="${w}" height="${h}" fill="#333" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
+</svg>`;
 
 const toBase64 = (str) =>
-  typeof window === 'undefined'
-    ? Buffer.from(str).toString('base64')
-    : window.btoa(str)
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
 
 const Shimmer = () => (
   <div>
@@ -33,6 +33,6 @@ const Shimmer = () => (
       height={475}
     />
   </div>
-)
+);
 
-export default Shimmer
+export default Shimmer;
